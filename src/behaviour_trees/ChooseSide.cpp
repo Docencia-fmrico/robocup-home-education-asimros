ef5068db3cfe73bdb5a9325d618de9fd6cf4e3d2
larapoves <poveslara@gmail.com>
2022-04-14 12:41:33 +0200

    .
    Signed-off-by: larapoves <poveslara@gmail.com>

---
+++ src/behaviour_trees/ChooseSide.cpp
@@ -0,0 +1,46 @@
+// Copyright 2019 Intelligent Robotics Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "behaviour_trees/ChooseSide.h"
+#include "behaviortree_cpp_v3/behavior_tree.h"
+#include <string>
+
+#include "ros/ros.h"
+
+namespace behaviour_trees
+{
+    ChooseSide::ChooseSide(const std::string& name)
+    : BT::ActionNodeBase(name, {})
+    {
+    }
+
+    void 
+    ChooseSide::halt()
+    {
+        ROS_INFO("ChooseSide halt");
+    }
+
+    BT::NodeStatus
+    ChooseSide::tick()
+    {
+        return BT::NodeStatus::SUCCESS;
+    }
+
+}  // namespace behaviour_trees
+
+#include "behaviortree_cpp_v3/bt_factory.h"
+BT_REGISTER_NODES(factory)
+{
+  factory.registerNodeType<behaviour_trees::ChooseSide>("ChooseSide");
+}
